8c69872b1c2fcea2ff18fbd92e8fc4106747cf73
SherryNinja10 <aarshdeepsinghvandal@gmail.com>
2024-10-27 12:37:28 -0700

    This has changed the way the nav bars work similar to demo 7. The skeleton js file was added and it also includes jqueary

---
+++ scripts/skeleton.js
@@ -0,0 +1,8 @@
+//---------------------------------------------------
+// This function loads the parts of your skeleton 
+// (navbar, footer, and other things) into html doc. 
+//---------------------------------------------------
+function loadSkeleton(){
+    console.log($('#navbarPlaceholder').load('./nav_before_login.html'));
+}
+loadSkeleton();  //invoke the function
