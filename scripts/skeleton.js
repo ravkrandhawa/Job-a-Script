f02c9b5febf774b19b5c0742f9195300bf225b95
SherryNinja10 <aarshdeepsinghvandal@gmail.com>
2024-10-27 23:48:25 -0700

    Added the authenication and all the ssteps that go with that

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -2,7 +2,18 @@
 // This function loads the parts of your skeleton 
 // (navbar, footer, and other things) into html doc. 
 //---------------------------------------------------
-function loadSkeleton(){
-    console.log($('#navbarPlaceholder').load('./nav_before_login.html'));
+function loadSkeleton() {
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {                   
+		        // If the "user" variable is not null, then someone is logged in
+            // User is signed in.
+            // Do something for the user here.
+            console.log($('#navbarPlaceholder').load('./nav_after_login.html'));
+        } else {
+            // No user is signed in.
+            console.log($('#navbarPlaceholder').load('./nav_before_login.html'));
+        }
+    });
 }
-loadSkeleton();  //invoke the function
+loadSkeleton(); //invoke the function
