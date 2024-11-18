7797a0b3fd31fb15b43b650298c02ce5fd11b5a3
SherryNinja10 <aarshdeepsinghvandal@gmail.com>
2024-11-18 11:24:18 -0800

    Added the book function into the display_matching.js. This then impacts the display_results.html page. Still need to work on making the bookmark highlighted when the page gets refereshed.

--- scripts/display_matching.js
+++ scripts/display_matching.js
@@ -83,27 +83,88 @@ function displayListings(listingsWithScores) {
     listingsWithScores.forEach(item => {
         const doc = item.listing;
         const matchScore = item.matchScore;
-
-        const jobCard = `
+        let jobCard = document.createElement("div");
+        jobCard.innerHTML = `
             <div class="col-md-4 mb-4">
-                <div class="card h-100">
-                    <img src="${doc.thumbnail || './images/default-thumbnail.png'}" class="card-img-top" alt="${doc.title}">
+                <div id="${doc.job_id}" class="card h-100">
+                    <img id="${doc.thumbnail}" src="${doc.thumbnail || './images/default-thumbnail.png'}" class="card-img-top" alt="${doc.title}">
                     <div class="card-body">
-                        <h5 class="card-title">${doc.title}</h5>
-                        <p class="card-text"><strong>Company:</strong> ${doc.company_name}</p>
+                        <h5 id="${doc.title}" class="card-title">${doc.title}</h5>
+                        <p id="${doc.company_name}" class="card-text"><strong>Company:</strong> ${doc.company_name}</p>
                         <p class="card-text"><strong>Location:</strong> ${doc.location}</p>
                         <p class="card-text">${doc.description ? doc.description.substring(0, 100) + '...' : ''}</p>
                         <p class="card-text"><strong>Match Score:</strong> ${matchScore}</p>
-                        <a href="${doc.share_link}" target="_blank" class="btn btn-primary">Apply Now</a>
-                        <i id="${doc.company_name}" class="material-icons float-end">bookmark_border</i>
+                        <a id="${doc.share_link}" href="${doc.share_link}" target="_blank" class="btn btn-primary">Apply Now</a>
+                        <i id="${doc.company_name}" class="material-icons float-end ${doc.job_id}">bookmark_border</i>
                     </div>
                 </div>
             </div>
         `;
 
-        contentDiv.innerHTML += jobCard;
+
+        contentDiv.appendChild(jobCard);
+
+        console.log(`#${doc.job_id}` + " doc ID")
+        // Attach event listener to the bookmark icon
+        const bookmarkIcon = document.getElementById(`#${doc.job_id}`);
+        console.log(bookmarkIcon);
+        jobCard.querySelector('i').onclick = () => {
+            // add if statement
+            bookmarkCollection(doc);
+        };
+
     });
 }
 
 // Run the userDataToArray function to start the process
 userDataToArray();
+
+function bookmarkCollection(doc) {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            // This are variables to get the id data that should be stored in firestore
+            // let thumbnail = document.querySelector('#${doc.thumbnail}');
+            console.log(doc)
+            let title = doc.title;
+            console.log(title);
+            // let companyName = document.querySelector('#${doc.company_name}');
+            // let shareLink = document.querySelector('#${doc.share_link}');
+
+            console.log(db.collection("users").doc(user.uid));
+
+            //extract details of "THE job"
+            var theJob = {
+                job_id: doc.job_id, 
+                // thumbnail: doc.thumbnail,
+                title: doc.title,
+                company_name: doc.company_name,
+                share_link: doc.share_link
+
+            }
+
+            // Reference to the user's subcollection "bookmarks"
+            let bookmarksRef = db.collection("users").doc(user.uid).collection("bookmarks");
+
+            // Add the job to the "bookmarks" subcollection
+            bookmarksRef.doc(theJob.job_id).set(theJob)
+                .then(() => {
+                    console.log("Bookmark added successfully!");
+                })
+                .catch(error => {
+                    console.error("Error adding bookmark: ", error);
+                });
+
+            console.log(`#${doc.job_id}` + " doc ID")
+            // Attach event listener to the bookmark icon
+            const bookmarkIcon = document.getElementById(`${doc.job_id}`);
+
+            console.log(bookmarkIcon);
+            bookmarkIcon.getElementsByClassName(doc.job_id)[0].innerHTML = 'bookmark';
+
+        } else {
+            // No user is signed in.
+            console.log("No user is signed in");
+            window.location.href = "login.html";
+        }
+    });
+}
