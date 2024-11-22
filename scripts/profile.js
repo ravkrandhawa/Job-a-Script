acbe712a981a238bc26758657f0b8d1131ca5d49
Nicholas Ng <nng37@my.bcit.ca>
2024-11-21 19:58:11 -0800

    Removed the placeholder images in our images folder, commented out unfunctional lines of code in the profile.js, adjusted the main.html part of the css to move the grid of the main items up, and added/created the logo for the main page.

--- scripts/profile.js
+++ scripts/profile.js
@@ -49,166 +49,166 @@ function saveUserInfo() {
 
 document.getElementById("personalInfoFields").disabled = true;
 
-function userDataToArray() {
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            currentUser = db.collection("users").doc(user.uid)
-            currentUser.get()
-                .then(userDoc => {
-                    // let userEducation = userDoc.data().education.trim();
-                    // let userEnvironment = userDoc.data().environment.trim();
-                    // let userExperience = userDoc.data().experience.trim();
-                    // let userFrameworks = userDoc.data().frameworks.trim();
-                    // let userJobType = userDoc.data().jobType.trim().toLowerCase();
-                    // let userLanguages = userDoc.data().languages.trim();
-                    // let userSalary = userDoc.data().salary.trim();
+// function userDataToArray() {
+//     firebase.auth().onAuthStateChanged(user => {
+//         if (user) {
+//             currentUser = db.collection("users").doc(user.uid)
+//             currentUser.get()
+//                 .then(userDoc => {
+//                     // let userEducation = userDoc.data().education.trim();
+//                     // let userEnvironment = userDoc.data().environment.trim();
+//                     // let userExperience = userDoc.data().experience.trim();
+//                     // let userFrameworks = userDoc.data().frameworks.trim();
+//                     // let userJobType = userDoc.data().jobType.trim().toLowerCase();
+//                     // let userLanguages = userDoc.data().languages.trim();
+//                     // let userSalary = userDoc.data().salary.trim();
                     
 
-                    const userInfo = {
-                        userEducation : userDoc.data().education.trim(),
-                        userEnvironment : userDoc.data().environment.trim(),
-                        userExperience : userDoc.data().experience.trim(),
-                        userFrameworks : userDoc.data().frameworks.trim(),
-                        userJobType : userDoc.data().jobType.trim().toLowerCase(),
-                        userLanguages : userDoc.data().languages.trim(),
-                        userSalary : userDoc.data().salary.trim()
-                    };
+//                     const userInfo = {
+//                         userEducation : userDoc.data().education.trim(),
+//                         userEnvironment : userDoc.data().environment.trim(),
+//                         userExperience : userDoc.data().experience.trim(),
+//                         userFrameworks : userDoc.data().frameworks.trim(),
+//                         userJobType : userDoc.data().jobType.trim().toLowerCase(),
+//                         userLanguages : userDoc.data().languages.trim(),
+//                         userSalary : userDoc.data().salary.trim()
+//                     };
 
-                    // Function to split each field's value into words
-function parseUserData(data) {
-    const parsedData = {};
+//                     // Function to split each field's value into words
+// function parseUserData(data) {
+//     const parsedData = {};
 
-    for (let key in data) {
-        // Split each string by spaces and filter out any empty strings
-        parsedData[key] = data[key].split(/\s+/).filter(Boolean);
-    }
+//     for (let key in data) {
+//         // Split each string by spaces and filter out any empty strings
+//         parsedData[key] = data[key].split(/\s+/).filter(Boolean);
+//     }
 
-    return parsedData;
-}
+//     return parsedData;
+// }
 
-// Parse the data
-const parsedUserData = parseUserData(userInfo);
-console.log(parsedUserData);
-// Function to split each field's value into words
-function parseUserData(data) {
-    const parsedData = {};
+// // Parse the data
+// const parsedUserData = parseUserData(userInfo);
+// console.log(parsedUserData);
+// // Function to split each field's value into words
+// function parseUserData(data) {
+//     const parsedData = {};
 
-    for (let key in data) {
-        // Split each string by spaces and filter out any empty strings
-        parsedData[key] = data[key].split(/\s+/).filter(Boolean);
-    }
+//     for (let key in data) {
+//         // Split each string by spaces and filter out any empty strings
+//         parsedData[key] = data[key].split(/\s+/).filter(Boolean);
+//     }
 
-    return parsedData;
-}
+//     return parsedData;
+// }
                     
-                    // let userInfo = ["SAP", "SQL Developer Intern", userEducation, userEnvironment, userExperience, userFrameworks, userJobType, userLanguages, userSalary];
-                    console.log(userInfo);
-                    fetch('http://127.0.0.1:8000')
-                    .then(response => response.json())
-                    .then(data => {
-                        // console.log(data); // Logs the data to the console
+//                     // let userInfo = ["SAP", "SQL Developer Intern", userEducation, userEnvironment, userExperience, userFrameworks, userJobType, userLanguages, userSalary];
+//                     console.log(userInfo);
+//                     fetch('http://127.0.0.1:8000')
+//                     .then(response => response.json())
+//                     .then(data => {
+//                         // console.log(data); // Logs the data to the console
             
-                        // const contentDiv = document.getElementById('content');
+//                         // const contentDiv = document.getElementById('content');
     
-                        let frequency = 0;
-                        data.forEach(doc => {
-                            if (isMatch(userInfo, doc)) {
-                                frequency++; 
-                            }
-                            company_name.push(doc.company_name);
+//                         let frequency = 0;
+//                         data.forEach(doc => {
+//                             if (isMatch(userInfo, doc)) {
+//                                 frequency++; 
+//                             }
+//                             company_name.push(doc.company_name);
 
-                            parseUserData(userInfo); 
-                            // salary.add();
+//                             parseUserData(userInfo); 
+//                             // salary.add();
             
-                            // if (doc.company_name === userInfo[0]) {
-                            //     frequency++;
-                            // }
+//                             // if (doc.company_name === userInfo[0]) {
+//                             //     frequency++;
+//                             // }
 
-                            // if (doc.title === userInfo[1]) {
-                            //     frequency++; 
-                            // }
+//                             // if (doc.title === userInfo[1]) {
+//                             //     frequency++; 
+//                             // }
             
-                            // message = message + "<div class=" + "card>" + "<img src=" + doc.thumbnail + "<p>" + "<span class=" + "job-title>"
-                            //     + doc.title + "</span>" +
-                            //     doc.company_name + "<br/>" + "<a href=" + doc.share_link
-                            //     + ">Apply Now!</a>" + "<i id=" + doc.company_name + " class=\"material-icons\">bookmark_border</i>" + "</p></div><br/>";
-                        })
+//                             // message = message + "<div class=" + "card>" + "<img src=" + doc.thumbnail + "<p>" + "<span class=" + "job-title>"
+//                             //     + doc.title + "</span>" +
+//                             //     doc.company_name + "<br/>" + "<a href=" + doc.share_link
+//                             //     + ">Apply Now!</a>" + "<i id=" + doc.company_name + " class=\"material-icons\">bookmark_border</i>" + "</p></div><br/>";
+//                         })
             
-                        console.log(frequency);
+//                         console.log(frequency);
             
-                        // Display the final string which includes all the jobs
-                        // contentDiv.innerHTML = message;
-                    })
+//                         // Display the final string which includes all the jobs
+//                         // contentDiv.innerHTML = message;
+//                     })
                     
 
-                })
+//                 })
 
-        } else {
-            console.log("No user data detected");
-        }
-    })
-}
+//         } else {
+//             console.log("No user data detected");
+//         }
+//     })
+// }
 
-// Function to split each field's value into words
-function parseUserData(data) {
-    const parsedData = {};
+// // Function to split each field's value into words
+// function parseUserData(data) {
+//     const parsedData = {};
 
-    for (let key in data) {
-        // Split each string by spaces and filter out any empty strings
-        parsedData[key] = data[key].split(/\s+/).filter(Boolean);
-    }
+//     for (let key in data) {
+//         // Split each string by spaces and filter out any empty strings
+//         parsedData[key] = data[key].split(/\s+/).filter(Boolean);
+//     }
 
-    return parsedData;
-}
+//     return parsedData;
+// }
 
-// Parse the data
-const parsedUserData = parseUserData(userInfo);
-console.log(parsedUserData);
-// Function to split each field's value into words
-function parseUserData(data) {
-    const parsedData = {};
+// // Parse the data
+// const parsedUserData = parseUserData(userInfo);
+// console.log(parsedUserData);
+// // Function to split each field's value into words
+// function parseUserData(data) {
+//     const parsedData = {};
 
-    for (let key in data) {
-        // Split each string by spaces and filter out any empty strings
-        parsedData[key] = data[key].split(/\s+/).filter(Boolean);
-    }
+//     for (let key in data) {
+//         // Split each string by spaces and filter out any empty strings
+//         parsedData[key] = data[key].split(/\s+/).filter(Boolean);
+//     }
 
-    return parsedData;
-}
+//     return parsedData;
+// }
 
 
 
-userDataToArray();
-function fetchJobs() {
-    fetch('http://127.0.0.1:8000')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data); // Logs the data to the console
+// userDataToArray();
+// function fetchJobs() {
+//     fetch('http://127.0.0.1:8000')
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data); // Logs the data to the console
 
-            const contentDiv = document.getElementById('content');
+//             const contentDiv = document.getElementById('content');
 
-            let company_name = [];
-            let salary = [];
-            data.forEach(doc => {
-                company_name.push(doc.company_name);
-                // salary.add();
+//             let company_name = [];
+//             let salary = [];
+//             data.forEach(doc => {
+//                 company_name.push(doc.company_name);
+//                 // salary.add();
 
-                let frequency = 0;
-                if (company_name.contains(userInfo[0])) {
-                    frequency++;
-                }
+//                 let frequency = 0;
+//                 if (company_name.contains(userInfo[0])) {
+//                     frequency++;
+//                 }
 
-                // message = message + "<div class=" + "card>" + "<img src=" + doc.thumbnail + "<p>" + "<span class=" + "job-title>"
-                //     + doc.title + "</span>" +
-                //     doc.company_name + "<br/>" + "<a href=" + doc.share_link
-                //     + ">Apply Now!</a>" + "<i id=" + doc.company_name + " class=\"material-icons\">bookmark_border</i>" + "</p></div><br/>";
-            })
+//                 // message = message + "<div class=" + "card>" + "<img src=" + doc.thumbnail + "<p>" + "<span class=" + "job-title>"
+//                 //     + doc.title + "</span>" +
+//                 //     doc.company_name + "<br/>" + "<a href=" + doc.share_link
+//                 //     + ">Apply Now!</a>" + "<i id=" + doc.company_name + " class=\"material-icons\">bookmark_border</i>" + "</p></div><br/>";
+//             })
 
-            console.log(frequency);
+//             console.log(frequency);
 
-            // Display the final string which includes all the jobs
-            contentDiv.innerHTML = message;
-        })
-        .catch(error => console.error('Error fetching data:', error));
-}
-// fetchJobs();
+//             // Display the final string which includes all the jobs
+//             contentDiv.innerHTML = message;
+//         })
+//         .catch(error => console.error('Error fetching data:', error));
+// }
+// // fetchJobs();
