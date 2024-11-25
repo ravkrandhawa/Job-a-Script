66d7655990691f303e0597512308ca7f15a5f400
Nicholas Ng <nng37@my.bcit.ca>
2024-11-24 22:14:40 -0800

    Finished styling for questionnaire.html and made a new file questionnaire.css in the styles folder. The js file for questionnaire has been optimized as the html originally was missing a lot of closing tags (oopsies).

--- scripts/questionnaire.js
+++ scripts/questionnaire.js
@@ -1,9 +1,10 @@
-// Function to store radio button answers
+// Function to store radio button and checkbox answers
 function storeRadioAnswers() {
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
             // Storing the experience level value
             let experience = document.querySelector('input[name="experience"]:checked')?.value || "";
+
             // Storing familiar languages
             let languagesA = document.querySelector('input[name="languagesA"]:checked')?.value || "";
             let languagesB = document.querySelector('input[name="languagesB"]:checked')?.value || "";
@@ -12,18 +13,23 @@ function storeRadioAnswers() {
             let languagesE = document.querySelector('input[name="languagesE"]:checked')?.value || "";
             let languagesF = document.querySelector('input[name="languagesF"]:checked')?.value || "";
             let languagesG = document.querySelector('input[name="languagesG"]:checked')?.value || "";
+
             // Storing minimum salary expectation
             let salary = document.querySelector('input[name="salary"]:checked')?.value || "";
+
             // Storing job type preference
             let fullTime = document.querySelector('input[name="full-time"]:checked')?.value || "";
             let partTime = document.querySelector('input[name="part-time"]:checked')?.value || "";
             let contract = document.querySelector('input[name="contract"]:checked')?.value || "";
+
             // Storing work environment preference
             let remote = document.querySelector('input[name="remote"]:checked')?.value || "";
             let hybrid = document.querySelector('input[name="hybrid"]:checked')?.value || "";
             let inPerson = document.querySelector('input[name="inPerson"]:checked')?.value || "";
+
             // Storing educational level
             let education = document.querySelector('input[name="education"]:checked')?.value || "";
+
             // Storing familiar frameworks
             let aws = document.querySelector('input[name="AWS"]:checked')?.value || "";
             let ajax = document.querySelector('input[name="AJAX"]:checked')?.value || "";
@@ -33,32 +39,29 @@ function storeRadioAnswers() {
             let playFramework = document.querySelector('input[name="playFramework"]:checked')?.value || "";
             let others = document.querySelector('input[name="Others"]:checked')?.value || "";
 
-        
             const db = firebase.firestore();
-            const questionnaireRef = db.collection("users").doc(user.uid)
-            console.log(questionnaireRef);
-            // We are setting the values of the questionnaire inputs to values in user's specific firebase database.
-            // These values are concatenated into strings and seperated by spaces.
+            const questionnaireRef = db.collection("users").doc(user.uid);
+
+            // Save data to Firestore
             questionnaireRef.set({
                 experience: experience,
-                languages: languagesA + " " + languagesB + " " + languagesC + " " + languagesD + " " + languagesE + " " + languagesF + " " + languagesG,
+                languages: `${languagesA} ${languagesB} ${languagesC} ${languagesD} ${languagesE} ${languagesF} ${languagesG}`.trim(),
                 salary: salary,
-                jobType: fullTime + " " + partTime + " " + contract,
-                environment: remote + " " + hybrid + " " + inPerson,
+                jobType: `${fullTime} ${partTime} ${contract}`.trim(),
+                environment: `${remote} ${hybrid} ${inPerson}`.trim(),
                 education: education,
-                frameworks: aws + " " + ajax + " " + angular + " " + bootstrap + " " + blazor + " " + playFramework + " " + others
-            }, {merge: true}).then(() => {
-                //Once the user hits submit, it will save the questionnaire details to firestore while also re-routing the user to the display_results.html page
-                window.location.href = "display_results.html"; 
-                //If there is an error with this re-routing, it will console log the error 
-            }).catch(error => {
-                console.error("Error saving data: ", error); 
-            })
-            
+                frameworks: `${aws} ${ajax} ${angular} ${bootstrap} ${blazor} ${playFramework} ${others}`.trim()
+            }, { merge: true })
+                .then(() => {
+                    // Redirect to display_results.html after successful submission
+                    window.location.href = "display_results.html";
+                })
+                .catch(error => {
+                    console.error("Error saving data: ", error);
+                });
         }
-    })
-
+    });
 }
 
-// Attach the function to your submit button
-document.querySelector('button[type="button"]').addEventListener('click', storeRadioAnswers);
+// Attach the function to the submit button
+document.querySelector('#submitQuestionnaire').addEventListener('click', storeRadioAnswers);
