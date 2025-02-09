a89db190f4e40b13ae5b80eaf1cf4bbf03f996cb
ravkrandhawa <rrandhawa59@my.bcit.ca>
2025-02-09 15:02:55 -0800

    update firebase key

--- scripts/firebaseAPI_BBY06.js
+++ scripts/firebaseAPI_BBY06.js
@@ -1,10 +1,11 @@
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB4tNm6m2iWH3IeCgIPKbNgYwFvuiDIg3C",
+  apiKey: "AIzaSyB4tNm6m2iWH3IeCgIPkbgNywfVuiDIg3c",
   authDomain: "job-a-script.firebaseapp.com",
   projectId: "job-a-script",
-  storageBucket: "job-a-script.appspot.com",
-  messagingSenderId: "77996872668",
-  appId: "1:77996872668:web:74d4788f13bf3dd1ad1d99",
+  storageBucket: "job-a-script.firebasestorage.app",
+  messagingSenderId: "777986872668",
+  appId: "1:777986872668:web:74d4788f13bf3dd1ad1d99"
 };
 
 const app = firebase.initializeApp(firebaseConfig);
